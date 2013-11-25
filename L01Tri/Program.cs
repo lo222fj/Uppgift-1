efa5debdf1d0eb7e11c6c7d1c99e4e573905f745
lo222fj <lo222fj@student.lnu.se>
2013-11-25 20:31:50 +0100

    Lagt till solution från VisualStudio

---
+++ L01Tri/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L01Tri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new double[] {};
+            Triangle testTriangel = new Triangle(array);
+        }
+    }
+}
---
+++ UnitTestL01Tri/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestL01Tri
+{
+    [TestClass]
+    public class TriangelUnitTest
+    {
+        [TestMethod]
+        public void isIsoscelesTest()
+        {
+            Triangle tri = new Triangle(1.0, 1.0, 0.5);
+            Assert.IsTrue(tri.isIsosceles());
+        }
+
+
+    }
+}
